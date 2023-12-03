7f0b236d7adc8072a1e551d0c51da83d1a9666fb
ArturDatsko <133085603+ArturDatsko@users.noreply.github.com>
2023-12-03 23:51:13 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import Header from './header/header';
 import { lazy, Suspense } from 'react';
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import { MainStyled } from './GlobalStyled';
 
 const HomePage = lazy(() => import('pages/home/home'));
@@ -40,18 +40,9 @@ export const App = () => {
             <Route path="cast" element={<MovieCast />} />
             <Route path="reviews" element={<MovieReviews />} />
           </Route>
+          {/* Використання "*" в підмаршруті, щоб відловлювати всі інші маршрути */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
-        <Route
-          path="*"
-          element={
-            <div>
-              <h2>Page not found</h2>
-              <p>
-                Back to <Link to="/">Home</Link>
-              </p>
-            </div>
-          }
-        />
       </Routes>
     </Suspense>
   );
